c4c48136cebc73113c649235614c152b5ba04021
Guest User <Guest@wts11.attlocal.net>
2015-07-22 18:34:38 -0700

    I worked on stuff

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,10 +1,66 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class Algorithms {
-public static int findBrokenEgg(List <String> eggs){
-	return 0;
-}
+	public static int findBrokenEgg(List<String> eggs) {
+		return eggs.indexOf("cracked");
+
+	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		int numOfPearls = 0;
+		for (Boolean b : oysters) {
+			if (b.booleanValue()) {
+				numOfPearls++;
+
+			}
+
+		}
+		return numOfPearls;
+	}
+
+	public static double findTallest(List<Double> peeps) {
+		double biggestHeight = 0;
+		for (Double d : peeps) {
+			if (d > biggestHeight) {
+				biggestHeight = d;
+			}
+		}
+		return biggestHeight;
+	}
+
+	public static String findLongestWord(List<String> words) {
+		String longestWord = "";
+		for (String s : words) {
+			if (s.length() > longestWord.length()) {
+				longestWord = s;
+			}
+
+		}
+		return longestWord;
+	}
+
+	public static boolean containsSOS(List<String> message1) {
+		boolean hasSOSMessage = false;
+		for (String s : message1) {
+			if (s.equals(" ... --- ... ")) {
+				hasSOSMessage = true;
+				
+				break;
+			}
+
+		}
+		
+		return hasSOSMessage;
+	}
+	public static List<Double> sortScores(List<Double> results){
+		List<Double> sortedList = new ArrayList<Double>();
+		sortedList.add(60.3);
+		for(Double d: results){
+			
+			
+		}
+		
+	}
 
 }
--- src/Level3TestWithAssertEqualsOhBabyATripleMLGPro420Blazin.java
+++ src/Level3TestWithAssertEqualsOhBabyATripleMLGPro420Blazin.java
@@ -16,48 +16,50 @@ public class Level3TestWithAssertEqualsOhBabyATripleMLGPro420Blazin {
 	}
 
 	/* Count how many oysters contain pearls. */
-//	@Test
-//	public void testSearchAndAccumulate() throws Exception {
-//		List<Boolean> oysters = Arrays.asList(new Boolean[] { false, false, true, false, false, false, false, false,
-//				false, false, false, false, false, false, false, false, false, false, false, false, false, false,
-//				false, true });
-//		assertEquals(2, Algorithms.countPearls(oysters));
-//	}
-//
+	@Test
+	public void testSearchAndAccumulate() throws Exception {
+		List<Boolean> oysters = Arrays.asList(new Boolean[] { false, false, true, false, false, false, false, false,
+				false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+				false, true });
+		assertEquals(2, Algorithms.countPearls(oysters));
+	}
+
 //	/* Create a method that will return the height of the tallest person. */
-//	@Test
-//	public void testFindMaxiumum() throws Exception {
-//		List<Double> peeps = Arrays.asList(new Double[] { 5.7, 6.2, 3.4, 6.2, 5.0, 5.5, 4.7, 6.2, 3.4, 4.2, 5.0, 4.5,
-//				5.1, 6.6 });
-//		assertEquals(6.6, Algorithms.findTallest(peeps));
-//	}
-//
+	@Test
+	public void testFindMaxiumum() throws Exception {
+		List<Double> peeps = Arrays.asList(new Double[] { 5.7, 6.2, 3.4, 6.2, 5.0, 5.5, 4.7, 6.2, 3.4, 4.2, 5.0, 4.5,
+				5.1, 6.6 });
+		assertEquals(6.6, Algorithms.findTallest(peeps),0);
+	}
+
 //	/* Create a method that will find the longest word. */
-//	@Test
-//	public void testFindMaxiumumString() throws Exception {
-//		List<String> words = Arrays.asList(new String[] { "accoutrements", "acumen", "anomalistic", "auspicious",
-//				"bellwether", "callipygian", "circumlocution", "concupiscent", "conviviality", "coruscant",
-//				"cuddlesome", "cupidity", "cynosure", "ebullient", "equanimity", "excogitate", "gasconading",
-//				"idiosyncratic", "luminescent", "magnanimous", "nidificate", "osculator", "parsimonious",
-//				"penultimate", "perfidiousness", "perspicacious", "proficuous", "remunerative", "saxicolous",
-//				"sesquipedalian", "superabundant", "unencumbered", "unparagoned", "usufruct", });
-//		assertEquals("circumlocution", Algorithms.findLongestWord(words));
-//	}
-//
+	@Test
+	public void testFindMaxiumumString() throws Exception {
+		List<String> words = Arrays.asList(new String[] { "accoutrements", "acumen", "anomalistic", "auspicious",
+				"bellwether", "callipygian", "circumlocution", "concupiscent", "conviviality", "coruscant",
+				"cuddlesome", "cupidity", "cynosure", "ebullient", "equanimity", "excogitate", "gasconading",
+				"idiosyncratic", "luminescent", "magnanimous", "nidificate", "osculator", "parsimonious",
+				"penultimate", "perfidiousness", "perspicacious", "proficuous", "remunerative", "saxicolous",
+				"sesquipedalian", "superabundant", "unencumbered", "unparagoned", "usufruct", });
+		assertEquals("circumlocution", Algorithms.findLongestWord(words));
+	}
+
 //	/*
 //	 * The Morse code for SOS is "· · · – – – · · ·". Create a method that can
 //	 * tell if SOS is contained in a message.
 //	 */
-//	@Test
-//	public void testFindString() throws Exception {
+	@Test
+	public void testFindString() throws Exception {
 //		List<String> message1 = Arrays.asList("".split(".... . .-. .-. --- / ... --- ... / -.-- .- .-.. .-.. ..."));
+//		String string1 = ".... . .-. .-. --- / ... --- ... / -.-- .- .-.. .-.. ...";
+//		List<String> message1 = Arrays.asList(string1.split("/"));
 //		assertEquals(true, Algorithms.containsSOS(message1));
-//		List<String> message2 = Arrays
-//				.asList(""
-//						.split(".-- .. - .... / - .... . / .-.. .. --. .... - ... / --- ..- - --..-- / .. - .----. ... / .-.. . ... ... / -.. .- -. --. . .-. --- ..- ... / .... . .-. . / .-- . / .- .-. . / -. --- .-- ? / . -. - . .-. - .- .. -. / ..- ... / .. / ..-. . . .-.. / ... - ..- .--. .. -.. / .- -. -.. / -.-. --- -. - .- --. .. --- ..- ... / .... . .-. . / .-- . / .- .-. . / -. --- .-- ? / . -. - . .-. - .- .. -. / ..- ... / .- / -- ..- .-.. .- - - --- --..-- / .- -. / .- .-.. -... .. -. --- --..-- / .- / -- --- ... --.- ..- .. - --- --..-- / -- -.-- / .-.. .. -... .. -.. --- / -.-- . .- .... --..-- / .... . -.-- / -.-- . .- ...."));
-//		assertEquals(false, Algorithms.containsSOS(message2));
-//	}
-//
+		List<String> message2 = Arrays
+				.asList(""
+						.split(".-- .. - .... / - .... . / .-.. .. --. .... - ... / --- ..- - --..-- / .. - .----. ... / .-.. . ... ... / -.. .- -. --. . .-. --- ..- ... / .... . .-. . / .-- . / .- .-. . / -. --- .-- ? / . -. - . .-. - .- .. -. / ..- ... / .. / ..-. . . .-.. / ... - ..- .--. .. -.. / .- -. -.. / -.-. --- -. - .- --. .. --- ..- ... / .... . .-. . / .-- . / .- .-. . / -. --- .-- ? / . -. - . .-. - .- .. -. / ..- ... / .- / -- ..- .-.. .- - - --- --..-- / .- -. / .- .-.. -... .. -. --- --..-- / .- / -- --- ... --.- ..- .. - --- --..-- / -- -.-- / .-.. .. -... .. -.. --- / -.-- . .- .... --..-- / .... . -.-- / -.-- . .- ...."));
+		assertEquals(false, Algorithms.containsSOS(message2));
+	}
+
 //	/* Sort the exam results from lowest to highest. */
 //	@Test
 //	public void testSort() throws Exception {
